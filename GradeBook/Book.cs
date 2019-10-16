9f0ca85d52983b0352c3387f80fb593ed3eeac15
Shashank Kuchibhotla <skuchibhotla@outlook.com>
2019-10-15 21:06:21 -0400

    Working with ctor code

---
+++ GradeBook/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<double> grades;
+    }
+}
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -10,6 +10,22 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+            book.AddGrade(89.1);
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result = result + number;
+            }
+
+            result = result / grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
+            Console.ReadLine();
         }
     }
 }
